c897e9514858ea70453bbd471904caa2b2f7ddc8
cmendible <cmendible@gmail.com>
2018-04-13 13:44:31 +0200

    Using ClusterIP for grafana and prometheus

--- 10.prometheus.grafana/deploy.ps1
+++ 10.prometheus.grafana/deploy.ps1
@@ -1,3 +1,5 @@
-helm install --name gbbhackprometheus stable/prometheus --version 4.6.13 -f prometheus-configforhelm.yaml
+helm install --name bootcampprometheus stable/prometheus --version 4.6.13 -f prometheus-configforhelm.yaml
 
-helm install --name gbbhackgrafana stable/grafana --version 0.5.1 --set server.service.type=LoadBalancer, server.adminUser=admin, server.adminPassword=admin, server.image=grafana/grafana:4.6.3, server.persistentVolume.enabled=false
+helm install --name bootcampgrafana stable/grafana --version 0.5.1 --set 'server.service.type=ClusterIP,server.adminUser=admin,server.adminPassword=admin,server.image=grafana/grafana:4.6.3,server.persistentVolume.enabled=false'
+
+kubectl apply -f ./ingress_rules.yaml
