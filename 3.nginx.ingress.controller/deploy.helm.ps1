1e1732a4000a6a7582ad30c3e3cceb8001e6c3a8
cmendible <cmendible@gmail.com>
2018-04-19 09:59:06 +0200

    changed helm script

--- 3.nginx.ingress.controller/deploy.helm.ps1
+++ 3.nginx.ingress.controller/deploy.helm.ps1
@@ -1,1 +1,4 @@
-helm install stable/nginx-ingress --name my-nginx --set controller.ingressClass=nginx
+helm install stable/nginx-ingress --name my-nginx `
+    --set controller.ingressClass=nginx `
+    --set controller.service.externalTrafficPolicy=Local `
+    --set controller.service.loadBalancerIP=127.0.0.1
